61c1936f09d42752f8997ca1dea67384ce49608d
nbabulkov <nbabulkov@gmail.com>
2020-03-28 17:06:50 +0200

    Refactor baseline
    Add more logging and error handling

--- elastic_search_baseline.py
+++ elastic_search_baseline.py
@@ -3,28 +3,45 @@ import json
 import numpy as np
 import pandas as pd
 import argparse
+from tqdm import tqdm
 from elasticsearch import Elasticsearch
 
 from lib.logger import logger
 
 PREDICT_FILE_COLUMNS = ['qid', 'Q0', 'docno', 'rank', 'score', 'tag']
+INDEX_NAME = 'vclaim'
+
+def create_connection(conn_string):
+    logger.debug("Starting ElasticSearch client")
+    try:
+        es = Elasticsearch([conn_string], sniff_on_start=True)
+    except:
+        raise ConnectionError(f"Couldn't connect to Elastic Search instance at: {conn_string} \
+                                Check if you've started it or if it listens on the port listed above.")
+    logger.debug("Elasticsearch connected")
+    return es
+
+def clear_index(es):
+    cleared = True
+    try:
+        es.indices.delete(index=INDEX_NAME)
+    except:
+        cleared = False
+    return cleared
 
 def build_index(es, vclaims, fieldnames):
-    try:
-        es.indices.delete(index='vclaim')
-    except:
-        pass
-
-    for i, vclaim in vclaims.iterrows():
-        if not es.exists(index='vclaim', id=i):
+    vclaims_count = vclaims.shape[0]
+    clear_index(es)
+    logger.info(f"Builing index of {vclaims_count} vclaims with fieldnames: {fieldnames}")
+    for i, vclaim in tqdm(vclaims.iterrows(), total=vclaims_count):
+        if not es.exists(index=INDEX_NAME, id=i):
             body = vclaim.loc[fieldnames].to_dict()
-            es.create(index='vclaim', id=i, body=body)
-    logger.info(f"Built index ({vclaims.shape[0]} vclaims) with fieldnames: {fieldnames}")
+            es.create(index=INDEX_NAME, id=i, body=body)
 
 def get_score(es, tweet, search_keys, size=10000):
     query = {"query": {"multi_match": {"query": tweet, "fields": search_keys}}}
     try:
-        response = es.search(index="vclaim", body=query, size=size)
+        response = es.search(index=INDEX_NAME, body=query, size=size)
     except:
         logger.error(f"No elasticsearch results for {tweet}")
         raise
@@ -39,20 +56,14 @@ def get_score(es, tweet, search_keys, size=10000):
     return df._score
 
 def get_scores(es, tweets, vclaims, search_keys, size):
-    size_tweets, size_vclaims = max(tweets.index), max(vclaims.index)
+    tweets_count, vclaims_count = max(tweets.index), max(vclaims.index)
     scores = {}
 
-    logger.info(f"Get RM5 scores for {size_tweets} tweets and {size_vclaims} vclaims")
-    for i, tweet in tweets.iterrows():
+    logger.info(f"Geting RM5 scores for {tweets_count} tweets and {vclaims_count} vclaims")
+    for i, tweet in tqdm(tweets.iterrows(), total=tweets_count):
         score = get_score(es, tweet.tweet_content, search_keys=search_keys, size=size)
         scores[i] = score
     return scores
-
-def create_connection(conn_string='127.0.0.1:9200'):
-    logger.debug("Start ElasticSearch listener")
-    es = Elasticsearch([conn_string])
-    logger.debug("Elasticsearch connected")
-    return es
 
 def format_scores(scores):
     formatted_scores = []
@@ -85,9 +96,9 @@ def main(args):
 
     es = create_connection(args.conn)
     build_index(es, vclaims, fieldnames=args.keys)
+    scores = get_scores(es, tweets, vclaims, search_keys=args.keys, size=args.size)
+    clear_index(es)
 
-    scores = get_scores(es, tweets, vclaims,
-                        search_keys=args.keys, size=args.size)
     formatted_scores = format_scores(scores)
     formatted_scores.to_csv(args.predict_file, sep='\t', index=False, header=False)
     logger.info(f"Saved scores from the model in file: {args.predict_file}")
