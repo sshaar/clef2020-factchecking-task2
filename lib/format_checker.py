036608a85010556044c268140c6ccc796c10e5ec
nbabulkov <nbabulkov@gmail.com>
2020-04-06 15:30:39 +0300

    Add checker for id duplication

--- lib/format_checker.py
+++ lib/format_checker.py
@@ -19,12 +19,21 @@ LINE_CHECKS = [
 
 def check_format(preditions_file_path):
     with open(preditions_file_path) as tsvfile:
+        pair_ids = {}
         for line_no, line_str in enumerate(tsvfile, start=1):
             line = line_str.split('\t')
             for check in LINE_CHECKS:
                 error = check(line)
                 if error is not None:
                     return f'{error} on line {line_no} in file: {preditions_file_path}'
+
+            tweet_id, vclaim_id = line[COLUMNS.index('qid')], line[COLUMNS.index('docno')]
+            duplication = pair_ids.get((tweet_id, vclaim_id), False)
+            if duplication:
+                return f'Duplication of pair(tweet_id={tweet_id}, vclaim_id={vclaim_id}) ' \
+                    f'on lines {duplication} and {line_no} in file: {preditions_file_path}'
+            else:
+                pair_ids[(tweet_id, vclaim_id)] = line_no
     return
 
 def parse_args():
